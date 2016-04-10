376f37d5b9c36c52401f9a55e07233ad6ea6923e
Jade Vance <jadenicolevance@gmail.com>
2016-04-09 19:03:44 -0700

    Muitple word scoring now works. CSS is commented out for testing purposes.

--- scrabble_site.rb
+++ scrabble_site.rb
@@ -1,7 +1,24 @@
 require 'sinatra'
 require_relative './lib/scoring.rb'
 
-class Scrabble < Sinatra::Base
+class Scrabble < Sinatra::Base		
+	
+	def get_score(word)
+		score = Scoring.score(word)
+	end
+
+	def get_scores(words)
+		scores_array = []
+		words.each do |word|
+			result = Scoring.score(word)
+			scores_array << result
+		end
+		return scores_array
+	end
+
+	get '/' do 
+		erb :index
+	end 
 
 	get '/score' do 
 		erb :score
@@ -9,20 +26,28 @@ class Scrabble < Sinatra::Base
 
   post '/score' do
     @word = params["word"]
-    @score = Scoring.score(params["word"])
+    @score = get_score(params["word"])
     erb :score
   end
 	
-	get '/' do 
-		erb :index
-	end 
 	get '/score-many' do 
 		erb :score_many
 	end 
 
-	get '/score' do 
-		erb :score
+	post '/score-many' do
+		words = []
+		params['number'].to_i.times do |i|
+			words << params["word-#{i}"]
+		end
+		if params.has_key? 'words'
+			@scores = []
+			@scores = get_scores(words)
+			@final_result = words.zip(@scores)
+		end 
+
+		erb :score_many
 	end 
 
+ 
 	run! 
 end
